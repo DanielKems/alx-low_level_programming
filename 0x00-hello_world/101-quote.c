2d1eda296eb701967eb0d6b85c1b8277e40c06cd
Daniel kems <deebek105@gmail.com>
2022-09-08 13:45:18 +0100

    Final

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,9 +7,7 @@
  */
 int main(void)
 {
-		int i = 1;
-			int b = 58;
 
-			write(i,  "and that piece of art is useful\" - Dora Korpar, 2015-10-19", b);
-					return (1);
+	write(2,  "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	return (1);
 }
