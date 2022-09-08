74eacef8a567426f5092071166d0865897cbdfa6
Daniel kems <deebek105@gmail.com>
2022-09-08 12:54:03 +0100

    101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <stdlib.h>
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ */
+int main(void)
+{
+		int i = 1;
+			int b = 58;
+
+write(i, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", b);
+return (1);
+}
