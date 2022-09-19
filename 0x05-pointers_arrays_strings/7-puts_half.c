d6f83c0991ffa353c6b0b764c79fa44fd2c97ac7
Daniel kems <deebek105@gmail.com>
2022-09-19 16:08:54 +0100

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -23,4 +23,13 @@ for (n = mid; n < length; n++)
 _putchar(str[n]);
 																					}
 }
+else
+{
+mid = (length - 1) / 2;
+for (n = mid + 1; n < length; n++)
+{
+_putchar(str[n]);
+																					}
+}
+_putchar('\n');
 }
