606f3870811cc1075b2d2b91baa29ff4e2f210f2
Daniel kems <deebek105@gmail.com>
2022-09-22 19:24:57 +0100

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
-
+/**
+ * Main: a program prints a[2] = 98
+ *
+ * Return: void
+ *
+ */
 int main(void)
-/* description - a program prints a[2] = 98 */
+
 {
-	  int n;
-	    int a[5];
-	      int *p;
+int n;
+int a[5];
+int *p;
 
 a[2] = 1024;
 p = &n;
-		    /*
-		     *    * write your line of code here...
-		     *       * Remember:
-		     *          * - you are not allowed to use a
-		     *             * - you are not allowed to modify p
-		     *                * - only one statement
-		     * * - you are not allowed to code anything else than this line of code
-		     */
+/*
+* write your line of code here...
+* Remember:
+* - you are not allowed to use a
+* - you are not allowed to modify p
+* - only one statement
+* - you are not allowed to code anything else than this line of code
+*/
 p[5] = 98;
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
